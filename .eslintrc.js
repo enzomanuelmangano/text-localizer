2e132a757b92b2842c2e98d54af1f3b1e9d31647
Enzo Manuel Mangano <enzomanuelmangano@gmail.com>
2021-09-08 16:36:10 +0200

    style: add linting & prettier rules

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['google', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    'require-jsdoc': 'off',
+  },
+};
